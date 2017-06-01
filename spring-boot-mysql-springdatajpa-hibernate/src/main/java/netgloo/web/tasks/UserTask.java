0de6140d79892696b9354cbede2c039533a1ca6b
Vincent Chen <vincent.chen@uilanetworks.com>
2017-06-01 15:39:45 +0800

    support hot deploy

--- spring-boot-mysql-springdatajpa-hibernate/src/main/java/netgloo/web/tasks/UserTask.java
+++ spring-boot-mysql-springdatajpa-hibernate/src/main/java/netgloo/web/tasks/UserTask.java
@@ -4,7 +4,6 @@ import netgloo.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -26,7 +25,7 @@ public class UserTask {
      * @Scheduled(fixedDelay = 5000) 上一次执行完毕时间点之后5秒再执行
      * @Scheduled(initialDelay=1000, fixedRate=5000) ：第一次延迟1秒后执行，之后按fixedRate的规则每5秒执行一次
      */
-    @Scheduled(fixedRate = 5000)
+    //@Scheduled(fixedRate = 5000)
     public void getOldestUser() {
         logger.info("the oldest user is {}", userService.getOldestUser());
     }
